0b93f6b6d9db1af03aa02149d8fa866ee9b94311
Matei Nicolae - Daniel <matei1337@icloud.com>
2023-03-01 15:49:13 +0200

    re-added selecting multiple clients based on inputs

--- src/templates/document-generator.php
+++ src/templates/document-generator.php
@@ -95,5 +95,35 @@
     </div>
 
     <script>
+        const inputFrom = document.getElementById('frm-from');
+        const inputTo = document.getElementById('frm-to');
+        const checkboxes = document.querySelectorAll('input[type="checkbox"]');
+
+        inputTo.addEventListener('change', () => {
+            const from = parseInt(inputFrom.value);
+            const to = parseInt(inputTo.value);
+
+            checkboxes.forEach((checkbox, i) => {
+                const clientId = parseInt(checkbox.value);
+
+                if (i >= from && i <= to) {
+                    checkboxes[i].checked = true;
+                } else {
+                    checkboxes[i].checked = false;
+                }
+
+                if (from > to) {
+                    checkboxes[i].checked = false;
+                }
+
+                if (from == 0 && to == 0) {
+                    checkboxes[i].checked = false;
+                }
+
+                if (from == 0 && to == checkboxes.length) {
+                    checkboxes[i].checked = true;
+                }
+            });
+        });
     </script>
 </body>
